d997eeb6a99ea365a2443ec018fb2bba46c53de1
ES, Choi <69956156+escape1001@users.noreply.github.com>
2024-03-29 16:08:15 +0900

    Create cowork_test.py

---
+++ cowork_test.py
@@ -0,0 +1,15 @@
+# [TO DO] 은선 본인 인사 print 하는 함수 작성
+def esc():
+    pass
+
+# [TO DO] 재철 본인 인사 print 하는 함수 작성
+def ljc():
+    pass
+
+# [TO DO] 민경 본인 인사 print 하는 함수 작성
+def jmk():
+    pass
+
+# [TO DO] 유선 본인 인사 print 하는 함수 작성
+def yys():
+    pass
